033a6c71c12d7e2ec0f9db6f1a9788a9746d2b1a
antena1thiago <thiago.carneiro@antena1.com.br>
2023-11-06 18:38:34 -0300

    feat: creatinh home

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['github.com'],
-    },
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+    ],
+  },
+}
+module.exports = nextConfig
